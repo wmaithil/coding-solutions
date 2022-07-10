981475ed16352f53f1f7a15ad528131e5fd8a5dc
wmaithil <wmaithil@gmail.com>
2022-07-11 00:23:56 +0530

    Create Max_books_allocation_LC-Hard.js

---
+++ Binary Search/Max_books_allocation_LC-Hard.js
@@ -0,0 +1,34 @@
+var splitArray = function(nums, m) {
+    let maxsum=0;
+    let minsum=0;
+    for(let i of nums){
+        maxsum += i;
+        minsum = Math.max(minsum, i);
+    }
+    if(m===1){
+        return maxsum;
+    }else if(m=== nums.length){
+        return minsum;  
+    }
+    while(minsum < maxsum){
+        let sum=0;
+        let pieces=1;
+        let mid= minsum + Math.floor((maxsum-minsum)/2);
+        for(let i=0;i<nums.length;i++){
+            if(sum+nums[i]> mid){
+                pieces+=1;
+                sum=nums[i];
+            }else{
+                sum = sum+nums[i];
+            }
+        }
+
+        if(pieces <= m ){
+            maxsum=mid;
+
+        }else{
+            minsum=mid+1;
+        }
+    }
+    return maxsum;
+};
